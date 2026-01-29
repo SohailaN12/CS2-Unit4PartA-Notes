d2ba79d025a07fae9ffd102ca11ad7a3de030fe0
SohailaN12 <sohaila.noori2027@gbwl.org>
2026-01-29 19:27:09 +0000

    4.3 notes

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+
+      // 1D ARRAY is a block of memory that stores a COLLECTION 
+      // of data items (elements) of the same type
+      // UNDER ONE VARIABLE NAME!!!
+
+
+      // How to DECLARE and CREATE an empty Array variable 
+      String[] apCompSci = new String[13];
+      double[] prices = new double[5]; // default values: 0.0
+      String[] names = new String[5]; // default values: null
+      int[] luckyNums = new int[10]; // default values: 0
+      boolean[] yesNo = new boolean[3]; // default values: false
+
+      // How to ACCESS an item in an Array
+      System.out.println(prices[0]);
+
+
+
+
+      
+   } // end Main METHOD
+} // end Main CLASS
